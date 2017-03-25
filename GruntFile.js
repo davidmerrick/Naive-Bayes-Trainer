7830d442e65bb413b269d53b29b07d93cb97a99e
David Merrick <david.merrick@jivesoftware.com>
2017-03-25 12:53:21 -0700

    Got hello world working

---
+++ GruntFile.js
@@ -0,0 +1,30 @@
+module.exports = grunt => {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            client: {
+                options: {
+                    transform: [['babelify', {presets: ['es2015', 'react']}]]
+                },
+                src: 'src/app/Client.jsx',
+                dest: 'dist/js/client-bundle.js'
+            }
+        },
+        watch: {
+            files: 'src/**',
+            tasks: ['default']
+        },
+        uglify: {
+            client: {
+                files: {
+                    'dist/js/client-bundle.js': ['dist/js/client-bundle.js']
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.registerTask('default', ['browserify', 'uglify', 'watch']);
+    grunt.registerTask('debug', ['browserify', 'watch']);
+};
