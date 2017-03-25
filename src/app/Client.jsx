46b7806f58d0f809451173da1adf2357f1f812d5
David Merrick <david.merrick@jivesoftware.com>
2017-03-25 13:00:08 -0700

    Added react-bootstrap buttons

--- src/app/Client.jsx
+++ src/app/Client.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'react-bootstrap'
 
 class Client extends React.Component {
     render(){
         return(
-            <h1>Hello world</h1>
+            <div>
+                <div>
+                    The text will go here.
+                </div>
+                <Button bsStyle="primary">Option 1</Button>    <Button bsStyle="primary">Option 2</Button>
+            </div>
         );
     }
 }
