817af2b13a01d7f56cea4dfd4aaa7437a1165f9d
David Merrick <david.merrick@jivesoftware.com>
2017-03-26 12:34:00 -0700

    Added endpoint for exporting classification

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import BodyParser from 'body-parser'
 import Endpoints from './src/constants/Endpoints'
 import Classification from './src/models/Classification'
+import bayes from 'bayes'
 
 const app = express();
 
@@ -9,13 +10,35 @@ app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(BodyParser.urlencoded({ extended: true }));
 
+let classifications = [];
+
+app.get(`${Endpoints.CLASSIFICATIONS}/count`, (req, res) => {
+    res.send(`${classifications.length}`);
+});
+
+app.get(`${Endpoints.CLASSIFICATIONS}/export`, (req, res) => {
+    let classifier = bayes();
+    classifications.forEach(item => {
+        classifier.learn(item.tweetText, item.option);
+    });
+
+    let classifierJson = classifier.toJson();
+
+    res.json(JSON.parse(classifierJson));
+});
+
+app.get(Endpoints.CLASSIFICATIONS, (req, res) => {
+    res.json(classifications);
+});
+
 app.post(Endpoints.CLASSIFICATIONS, (req, res) => {
     let tweetText = req.body.tweetText;
     let option = req.body.option;
 
-    let myClassification = new Classification(tweetText, option);
+    let newClassification = new Classification(tweetText, option);
+    classifications.push(newClassification);
 
-    res.json(myClassification);
+    res.json(newClassification);
 });
 
 const PORT = 3000;
