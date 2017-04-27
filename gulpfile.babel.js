639bd0c9a87763944ba38933f0df249ba1288743
david.merrick <david.merrick@jivesoftware.com>
2017-04-27 15:19:57 -0700

    Got browserify working with gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,19 +1,37 @@
 'use strict';
 
-import gulp from 'gulp'
-import gUtil from 'gulp-util'
-import browserify from 'gulp-browserify'
-import reactify from 'reactify'
+import gulp from "gulp";
+import gutil from "gulp-util";
+import browserify from "browserify";
+import source from "vinyl-source-stream";
+import buffer from "vinyl-buffer";
+import uglify from "gulp-uglify";
+import sourcemaps from "gulp-sourcemaps";
 
 const SRC = "src/app/Root.jsx";
 const DEST = "public/js/bundle.js";
 
-gulp.task('browserify', () => {
-    gulp.src(SRC)
-        .pipe(browserify({
-            transform: [reactify]
-        }))
-        .pipe(gulp.dest(DEST))
+gulp.task('javascript', () => {
+    // set up the browserify instance on a task basis
+    let b = browserify({
+        entries: SRC,
+        debug: true
+    });
+
+    return b
+        .transform("babelify", {
+            presets: ["es2015", "react"],
+            plugins: ['transform-decorators-legacy', 'transform-object-rest-spread']
+        })
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(DEST));
 });
 
-gulp.task('default', ['browserify'])
+gulp.task('default', ['javascript'])
