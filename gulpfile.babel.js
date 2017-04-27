4e61ef16f4dd2340d579382c0f70bb39a4d62654
David Merrick <david.merrick@jivesoftware.com>
2017-04-27 13:35:36 -0700

    Working on getting Grunt tasks ported over to Gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,18 @@
 
 import gulp from 'gulp'
 import gUtil from 'gulp-util'
+import browserify from 'gulp-browserify'
+import reactify from 'reactify'
+
+const SRC = "src/app/Root.jsx";
+const DEST = "public/js/bundle.js";
 
 gulp.task('browserify', () => {
-    gUtil.log("hello world");
+    gulp.src(SRC)
+        .pipe(browserify({
+            transform: [reactify]
+        }))
+        .pipe(gulp.dest(DEST))
 });
 
 gulp.task('default', ['browserify'])
